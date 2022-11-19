8a5a9b3bc4411887a3dcaaa748da7e66b9ed4ac1
Crowdbotics <team@crowdbotics.com>
2022-11-19 21:08:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lilburn_shoe_lift_c_37640",
+  url: "https://lilburn_shoe_lift_c_37640.botics.co",
+  api: "https://lilburn_shoe_lift_c_37640.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
